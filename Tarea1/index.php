0645b9f3f7ec10da640e4ab47f08be80e085dc73
Carlos Oliva <colidom@outlook.com>
2023-10-04 19:28:09 +0200

    Ejercicio javascript

---
+++ Tarea1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change Image</title>
+</head>
+
+<body>
+    <p>Click sobre las letas para cambiarlas</p>
+    <img id="myFPImage" src="http://myfpschool.com/wp-content/uploads/2016/06/myblack.jpeg" alt="Image" width="100" height="100" onClick="cambiaPic()">
+
+    <script>
+        function cambiaPic() {
+            var image = document.getElementById("myFPImage");
+            if (image.src.match("green")) {
+                image.src = "http://myfpschool.com/wp-content/uploads/2016/06/myblack.jpeg";
+            } else {
+                image.src = "http://myfpschool.com/wp-content/uploads/2016/06/mygreen.jpeg"
+            }
+        }
+    </script>
+</body>
+
+</html>
