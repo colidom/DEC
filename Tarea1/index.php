213139a13cd24cbe0695061f53853f90141d6231
Carlos Oliva <colidom@outlook.com>
2023-10-04 19:34:11 +0200

    Saltos de línea

--- Tarea1/index.php
+++ Tarea1/index.php
@@ -10,8 +10,6 @@
 <body>
     <p>Click sobre las letas para cambiarlas</p>
     <img id="myFPImage" src="http://myfpschool.com/wp-content/uploads/2016/06/myblack.jpeg" alt="Image" width="100" height="100" onClick="cambiaPic()">
-
-
 </body>
 
 </html>
