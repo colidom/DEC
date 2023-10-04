c0727cce7254b369379a5e672b80026b31ef1db9
Carlos Oliva <colidom@outlook.com>
2023-10-04 19:52:56 +0200

    Ejercicio2

---
+++ Tarea2/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change Image</title>
+</head>
+
+<body>
+    <p>Click en la imágen para cambiar su tamaño</p>
+    <img id="myFPImage" src="http://myfpschool.com/wp-content/uploads/2016/01/monkey-tablet.jpg" alt="Image" width="100" height="100" onClick="cambiaPic()">
+</body>
+
+</html>
+<script>
+    function cambiaPic() {
+
+        var image = document.getElementById("myFPImage");
+        width = image.getAttribute("width");
+        height = image.getAttribute("height");
+
+        if (width == 100 && height == 100) {
+            image.setAttribute("width", "500");
+            image.setAttribute("height", "500");
+        } else {
+            image.setAttribute("width", "100");
+            image.setAttribute("height", "100");
+        }
+    }
+</script>
