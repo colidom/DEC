31d55e5dee4001b3cb25b9653410809d2b8c2651
Carlos Oliva <colidom@outlook.com>
2023-10-04 20:09:21 +0200

    Añadido otro ejemplo

--- Tarea2/index.php
+++ Tarea2/index.php
@@ -10,6 +10,9 @@
 <body>
     <p>Click en la imágen para cambiar su tamaño</p>
     <img id="myFPImage" src="http://myfpschool.com/wp-content/uploads/2016/01/monkey-tablet.jpg" alt="Image" width="100" height="100" onClick="cambiaPic()">
+    <hr>
+    <img id="container" src='http://myfpschool.com/wp-content/uploads/2016/01/monkey-tablet.jpg' alt="Image" width="100" height="100" />
+    <button>Resize</button>
 </body>
 
 </html>
@@ -26,6 +29,17 @@
         } else {
             image.setAttribute("width", "100");
             image.setAttribute("height", "100");
+        }
+    }
+
+    document.querySelector('button').onclick = function() {
+        var image = document.getElementById('container');
+        if (image.style.width < '500px') {
+            image.style.width = '500px';
+            image.style.height = 'auto';
+        } else {
+            image.style.width = '100px';
+
         }
     }
 </script>
