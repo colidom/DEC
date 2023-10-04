b22ef6ea21f4bed96aabb7e27a1d09f14a6d2eaa
Carlos Oliva <colidom@outlook.com>
2023-10-04 19:32:16 +0200

    Se mueve script fuera de body

--- Tarea1/index.php
+++ Tarea1/index.php
@@ -11,16 +11,18 @@
     <p>Click sobre las letas para cambiarlas</p>
     <img id="myFPImage" src="http://myfpschool.com/wp-content/uploads/2016/06/myblack.jpeg" alt="Image" width="100" height="100" onClick="cambiaPic()">
 
-    <script>
-        function cambiaPic() {
-            var image = document.getElementById("myFPImage");
-            if (image.src.match("green")) {
-                image.src = "http://myfpschool.com/wp-content/uploads/2016/06/myblack.jpeg";
-            } else {
-                image.src = "http://myfpschool.com/wp-content/uploads/2016/06/mygreen.jpeg"
-            }
-        }
-    </script>
+
 </body>
+
+<script>
+    function cambiaPic() {
+        var image = document.getElementById("myFPImage");
+        if (image.src.match("green")) {
+            image.src = "http://myfpschool.com/wp-content/uploads/2016/06/myblack.jpeg";
+        } else {
+            image.src = "http://myfpschool.com/wp-content/uploads/2016/06/mygreen.jpeg"
+        }
+    }
+</script>
 
 </html>
