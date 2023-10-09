8baf454238f730aa2b8697f24884fd5d98cf79d1
Carlos Oliva <colidom@outlook.com>
2023-10-09 19:16:01 +0200

    Tarea3

---
+++ Tarea3/assets/js/script.js
@@ -0,0 +1,15 @@
+function fadeWindow() {
+    /* var image = document.getElementById("myFPImage");
+    width = image.getAttribute("width");
+    height = image.getAttribute("height");
+
+    if (width == 100 && height == 100) {
+        image.setAttribute("width", "500");
+        image.setAttribute("height", "500");
+    } else {
+        image.setAttribute("width", "100");
+        image.setAttribute("height", "100");
+    } */
+    let window = document.getElementById("#window");
+    window.fadeOut("slow");
+}
---
+++ Tarea3/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halloween Gift</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="col-sm-12">
+            <div class="col-sm-6">
+                <div class="content text-center texto">
+                    <p>!Truco o Trato! FELICIDADES te hemos elegido para recibir un fantástico regalo por estas terroríficas fiestas, no lo dejes escapar...</p>
+                    <p>¡Feliz Hallowen!</p>
+                </div>
+            </div>
+        </div>
+        <!-- Button trigger modal -->
+        <div class="col text-center">
+            <button type="button" class="btn btn-warning main-button" onClick="fadeWindow()">
+                Reclamar regalo
+            </button>
+        </div>
+    </div>
+
+    <div id="window">
+        <p>Esta es la img</p>
+    </div>
+</body>
+
+</html>
+<script src="./assets/js/script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
