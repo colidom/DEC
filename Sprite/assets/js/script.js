5c86a83596d5a0f9a3cb0cfece16aab662a929e4
colidom <colidom@outlook.com>
2024-01-18 18:44:27 +0100

    Haciendo que la animación se mueva por la pantalla

--- Sprite/assets/js/script.js
+++ Sprite/assets/js/script.js
@@ -13,14 +13,35 @@ const height = 18;
 const scaledWidth = scale * width;
 const scaledHeight = scale * height;
 
-function drawFrame(frameX, frameY, canvasX, canvasY) {
-    ctx.drawImage(img, frameX * width, frameY * height, width, height, canvasX, canvasY, scaledWidth, scaledHeight);
-}
-
 const cycleLoop = [0, 1, 0, 2];
 let currentLoopIndex = 0;
 let frameCount = 0;
 let currentDirection = 0;
+
+const sprite = {
+    x: Math.random() * (canvas.width - scaledWidth),
+    y: Math.random() * (canvas.height - scaledHeight),
+    speedX: 6.5,
+    speedY: 6.5,
+};
+
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+    ctx.drawImage(img, frameX * width, frameY * height, width, height, canvasX, canvasY, scaledWidth, scaledHeight);
+}
+
+function moveSprite() {
+    sprite.x += sprite.speedX;
+    sprite.y += sprite.speedY;
+
+    // Limitar el sprite dentro del canvas
+    if (sprite.x < 0 || sprite.x + scaledWidth > canvas.width) {
+        sprite.speedX *= -1; // Invertir la velocidad en el eje X al llegar al borde izquierdo o derecho
+    }
+
+    if (sprite.y < 0 || sprite.y + scaledHeight > canvas.height) {
+        sprite.speedY *= -1; // Invertir la velocidad en el eje Y al llegar al borde superior o inferior
+    }
+}
 
 function step() {
     frameCount++;
@@ -30,16 +51,21 @@ function step() {
     }
     frameCount = 0;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawFrame(cycleLoop[currentLoopIndex], currentDirection, 0, 0);
+
+    // Mueve y dibuja el sprite principal
+    drawFrame(cycleLoop[currentLoopIndex], currentDirection, sprite.x, sprite.y);
     currentLoopIndex++;
     if (currentLoopIndex >= cycleLoop.length) {
         currentLoopIndex = 0;
-        currentDirection++; // Next row/direction in the sprite sheet
+        currentDirection++;
     }
-    // Reset to the "down" direction once we've run through them all
     if (currentDirection >= 4) {
         currentDirection = 0;
     }
+
+    // Mueve el sprite principal
+    moveSprite();
+
     window.requestAnimationFrame(step);
 }
 
