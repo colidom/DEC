597c7199a52c496e3f01b9b1fe94e8c6289fdd49
colidom <colidom@outlook.com>
2024-01-18 18:53:12 +0100

    Implementado segundo personaje y se permite moverlo con las flechas del teclado

--- Sprite/assets/js/script.js
+++ Sprite/assets/js/script.js
@@ -21,8 +21,15 @@ let currentDirection = 0;
 const sprite = {
     x: Math.random() * (canvas.width - scaledWidth),
     y: Math.random() * (canvas.height - scaledHeight),
-    speedX: 6.5,
-    speedY: 6.5,
+    speedX: 2.5,
+    speedY: 2.5,
+};
+
+const player = {
+    x: canvas.width / 2 - scaledWidth / 2,
+    y: canvas.height / 2 - scaledHeight / 2,
+    speed: 2.5,
+    velocity: { x: 0, y: 0 },
 };
 
 function drawFrame(frameX, frameY, canvasX, canvasY) {
@@ -33,7 +40,7 @@ function moveSprite() {
     sprite.x += sprite.speedX;
     sprite.y += sprite.speedY;
 
-    // Limitar el sprite dentro del canvas
+    // Limitar el sprite dentro del canvas y evitar esquinas
     if (sprite.x < 0 || sprite.x + scaledWidth > canvas.width) {
         sprite.speedX *= -1; // Invertir la velocidad en el eje X al llegar al borde izquierdo o derecho
     }
@@ -41,6 +48,37 @@ function moveSprite() {
     if (sprite.y < 0 || sprite.y + scaledHeight > canvas.height) {
         sprite.speedY *= -1; // Invertir la velocidad en el eje Y al llegar al borde superior o inferior
     }
+}
+
+function movePlayer() {
+    document.addEventListener("keydown", (event) => {
+        switch (event.key) {
+            case "ArrowLeft":
+                player.velocity.x = -player.speed;
+                player.velocity.y = 5;
+                break;
+            case "ArrowRight":
+                player.velocity.x = player.speed;
+                player.velocity.y = 5;
+                break;
+            case "ArrowUp":
+                player.velocity.x = 5;
+                player.velocity.y = -player.speed;
+                break;
+            case "ArrowDown":
+                player.velocity.x = 5;
+                player.velocity.y = player.speed;
+                break;
+        }
+    });
+
+    // Actualizar la posición del jugador
+    player.x += player.velocity.x;
+    player.y += player.velocity.y;
+
+    // Limitar al jugador dentro del canvas
+    player.x = Math.max(0, Math.min(canvas.width - scaledWidth, player.x));
+    player.y = Math.max(0, Math.min(canvas.height - scaledHeight, player.y));
 }
 
 function step() {
@@ -65,6 +103,10 @@ function step() {
 
     // Mueve el sprite principal
     moveSprite();
+
+    // Mueve al jugador controlado por el teclado
+    movePlayer();
+    drawFrame(1, 0, player.x, player.y);
 
     window.requestAnimationFrame(step);
 }
