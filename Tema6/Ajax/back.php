396d068af48c353581599e545048d1177ad59c34
colidom <colidom@outlook.com>
2024-01-26 15:39:48 +0100

    Añadido else para aportar más información en el front

--- Tema6/Ajax/back.php
+++ Tema6/Ajax/back.php
@@ -1,7 +1,11 @@
 <?php
 $datosCliente = isset($_POST['datos']) ? $_POST['datos'] : '';
 
-$resultado = 'El servidor recibió: ' . $datosCliente;
+if ($datosCliente) {
+    $resultado = 'El servidor recibió: ' . $datosCliente;
+} else {
+    $resultado = "El servidor recibió la respuesta pero no se han enviado datos en el formulario";
+}
 
 header('Content-Type: application/json');
 echo json_encode(['resultado' => $resultado]);
