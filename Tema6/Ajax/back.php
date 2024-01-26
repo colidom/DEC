c1c486344078154cee09b5d584158a1d6bfb9e30
colidom <colidom@outlook.com>
2024-01-26 15:46:42 +0100

    Cambiado texto para dar maor info

--- Tema6/Ajax/back.php
+++ Tema6/Ajax/back.php
@@ -4,7 +4,7 @@ $datosCliente = isset($_POST['datos']) ? $_POST['datos'] : '';
 if ($datosCliente) {
     $resultado = 'El servidor recibió: ' . $datosCliente;
 } else {
-    $resultado = "El servidor recibió la respuesta pero no se han enviado datos en el formulario";
+    $resultado = "El servidor recibió la respuesta pero no se han informado datos en el formulario";
 }
 
 header('Content-Type: application/json');
