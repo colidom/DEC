c122e8cdcd369108901c093fb9df37cbb348b6b5
colidom <colidom@outlook.com>
2024-02-01 18:53:03 +0100

    Update Ajax and Fetch functions to use SweetAlert2 for error handling and add checks for empty input.

--- Tema6/Ajax/script.js
+++ Tema6/Ajax/script.js
@@ -17,55 +17,71 @@ $(document).ready(function () {
 function realizarLlamadaAjax() {
     var datos = $("#datosInputAjax").val();
 
-    $.ajax({
-        type: "POST",
-        url: "back.php",
-        data: { datos: datos },
-        dataType: "json",
-        success: function (respuestaJSON) {
-            $("#respuestaAjax").html(`<p>${respuestaJSON.resultado}</p>`);
+    if (datos) {
+        $.ajax({
+            type: "POST",
+            url: "back.php",
+            data: { datos: datos },
+            dataType: "json",
+            success: function (respuestaJSON) {
+                $("#respuestaAjax").html(`<p>${respuestaJSON.resultado}</p>`);
 
-            // Oculta el bloque después de 10 segundos
-            setTimeout(function () {
+                // Oculta el bloque después de 10 segundos
+                setTimeout(function () {
+                    $("#respuestaAjax").hide();
+                }, 10000);
+            },
+            error: function () {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Error en la llamada AJAX!",
+                });
                 $("#respuestaAjax").hide();
-            }, 10000);
-        },
-        error: function () {
-            alert("Error en la llamada AJAX");
-        },
-    });
-
-    $("#respuestaAjax").show();
+            },
+        });
+        $("#respuestaAjax").show();
+    } else {
+        Swal.fire("No se ha proporcionado ningún dato para la llamada Ajax.");
+    }
 }
 
 // Función Fetch
 function realizarLlamadaFetch() {
     var datos = $("#datosInputFetch").val();
 
-    fetch("back.php", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: "datos=" + encodeURIComponent(datos),
-    })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("La respuesta del servidor no fue exitosa");
-            }
-            return response.json();
+    if (datos) {
+        fetch("back.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: "datos=" + encodeURIComponent(datos),
         })
-        .then((respuestaJSON) => {
-            $("#respuestaFetch").html(`<p>${respuestaJSON.resultado}</p>`);
-            $("#respuestaFetch").show();
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("La respuesta del servidor no fue exitosa");
+                }
+                return response.json();
+            })
+            .then((respuestaJSON) => {
+                $("#respuestaFetch").html(`<p>${respuestaJSON.resultado}</p>`);
+                $("#respuestaFetch").show();
 
-            // Oculta el bloque después de 10 segundos
-            setTimeout(function () {
-                $("#respuestaFetch").hide();
-            }, 10000);
-        })
-        .catch((error) => {
-            alert("Error en la llamada Fetch: " + error.message);
-            console.error(error);
-        });
+                // Oculta el bloque después de 10 segundos
+                setTimeout(function () {
+                    $("#respuestaFetch").hide();
+                }, 10000);
+            })
+            .catch((error) => {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Error en la llamada Fetch: " + error.message,
+                });
+                console.error(error);
+            });
+    } else {
+        Swal.fire("No se ha proporcionado ningún dato para la llamada Fetch.");
+    }
 }
